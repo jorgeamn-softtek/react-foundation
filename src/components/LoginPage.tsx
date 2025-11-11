3cb7745d8fa0fbb37d42817f6cc6254102967cf2
Jorge Mendizabal <jorge.mendizabal@softtek.com>
2025-11-11 07:24:46 -0600

    clase zustando login logout

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
 import { useAuthStore } from "../store/auth.store";
 
 export const LoginPage = () => {
   const authStatus = useAuthStore((state) => state.status);
+  const login = useAuthStore((state) => state.login);
+  const logout = useAuthStore((state) => state.logout);
+  const user = useAuthStore((state) => state.user);
 
-  return <h2>Login status {authStatus}</h2>;
+  useEffect(() => {
+    setTimeout(() => {
+      logout();
+    }, 1500);
+  });
+
+  if (authStatus === "checking") {
+    return <h2>Loading</h2>;
+  }
+
+  return (
+    <>
+      <h3>Login page</h3>
+      {authStatus === "authenticated" ? (
+        <div>Autenticado como: {JSON.stringify(user, null, 2)}</div>
+      ) : (
+        <div>No autenticado</div>
+      )}
+
+      {authStatus === "authenticated" ? (
+        <button onClick={() => logout()}>Logout</button>
+      ) : (
+        <button onClick={() => login("jorge@me.com", "pass")}>Login</button>
+      )}
+    </>
+  );
 };
--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -7,10 +7,32 @@ interface AuthState {
     name: string;
     email: string;
   };
+
+  login: (email: string, password: string) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
   status: "checking",
   token: undefined,
   user: undefined,
+
+  login: (email: string, password: string) => {
+    set({
+      status: "authenticated",
+      token: "ABC123",
+      user: {
+        name: "Jorge Mendizabal",
+        email: "jorge@jorge.com",
+      },
+    });
+  },
+
+  logout: () => {
+    set({
+      status: "not-authenticated",
+      token: undefined,
+      user: undefined,
+    });
+  },
 }));
